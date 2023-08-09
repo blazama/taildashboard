ffd4850728a2a39c349b6dedfb7f74b57545538f
blazama <blancazapicom@gmail.com>
2023-08-09 14:28:57 +0200

    toggle dark mode logic

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  darkMode: boolean = false;
 
+  @HostBinding('class') get themeMode() {
+    return this.darkMode ? 'dark' : 'light';
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+  }
 }
