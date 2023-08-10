4b26e089a3c5ff30e0b1c3a20f6d1ee50295a10a
blazama <blancazapicom@gmail.com>
2023-08-10 16:34:11 +0200

    api service logic

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private urlApi = 'https://randomuser.me/api/';
+
+  constructor(private http: HttpClient) { }
+
+  public getData(): Observable<[]> {
+    return this.http.get<[]>(this.urlApi);
+  }
 }
