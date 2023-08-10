4948f71a20e2361ee79eb2266f17cbf39dea092d
blazama <blancazapicom@gmail.com>
2023-08-10 18:27:00 +0200

    user logic implements interval and api call

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, switchMap, timer } from 'rxjs';
+import { Data } from 'src/app/interfaces/data';
+import { ApiService } from 'src/app/service/api.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+export class UserComponent implements OnInit, OnDestroy {
 
+  data: Data;
+  name: string;
+  surname: string;
+  picture:string;
+  private userSubscription: Subscription;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.getUserData();
+  }
+
+  resetUser() {
+    this.getUserData()
+  }
+
+  getUserData() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+
+    this.userSubscription = timer(0, 10000)
+      .pipe(
+        switchMap(() => this.apiService.getData())
+      )
+      .subscribe(data => {
+        this.name = data.results[0].name.first;
+        this.surname = data.results[0].name.last;
+        this.picture = data.results[0].picture.thumbnail;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 }
