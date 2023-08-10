5cf5dd539a7c4765cb1364b10da515f407291307
blazama <blancazapicom@gmail.com>
2023-08-10 18:26:03 +0200

    api service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Observable } from 'rxjs';
+import { Data } from '../interfaces/data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private urlApi = 'https://randomuser.me/api/';
+  private urlApi = 'https://randomuser.me/api?results=1';
 
   constructor(private http: HttpClient) { }
 
-  public getData(): Observable<[]> {
-    return this.http.get<[]>(this.urlApi);
+  public getData(): Observable<Data> {
+    return this.http.get<Data>(this.urlApi);
   }
 }
