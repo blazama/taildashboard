52866f3ea2d3dbb93d0229299d8ef7c46a8e6adb
blazama <blancazapicom@gmail.com>
2023-08-10 17:30:44 +0200

    user interface

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    name: {
+      title: string;
+      first: string;
+      last: string;
+    };
+    phone: string;
+    picture: {
+      thumbnail: string;
+    };
+  }
+  
